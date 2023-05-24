bbf82c7d7e0825a10d186e566983f06552819a19
Sandra López Iglesias <slopeziglesias@danielcastelao.org>
2023-05-24 11:38:41 +0200

    actualizando javadoc de la clase Controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,18 +1,40 @@
 public class Controller {
+    /**
+     * Llama al metodo view() de la clase View
+     * @param args
+     */
     public static void main(String[] args) {
         View.View();
     }
 
+    /**
+     * Metodo que llama al metodo crearCoche() de la clase Model
+     * @param modelo del coche
+     * @param matricula del coche
+     */
     public static void crearButton(String modelo, String matricula) {
         Coche aux = Model.crearCoche(modelo, matricula);
         if (aux != null) View.muestraVelocidad(aux.matricula, aux.velocidad);
     }
 
+    /**
+     * Metodo que llama al metodo subirVelocidad() de la clase Model
+     * @param matricula del coche
+     * @param v velocidad del coche
+     * @return un objeto Integer que indica la velocidad
+     */
     public static Integer subirButton(String matricula, Integer v) {
         Integer aux = Model.subirVelocidad(matricula, v);
         if (aux != null) View.muestraVelocidad(matricula, aux);
         return aux;
     }
+
+    /**
+     * Metodo que llama al metodo bajarVelocidad() de la clase Model
+     * @param matricula del coche
+     * @param v velocidad del coche
+     * @return un objeto Integer que indica la velocidad
+     */
     public static Integer bajarButton(String matricula, Integer v) {
         Integer aux = Model.bajarVelocidad(matricula, v);
         if (aux != null) View.muestraVelocidad(matricula, aux);
