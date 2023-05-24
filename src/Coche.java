bd581ae1f39b122bc01ea49979e117a926c4445d
Sandra López Iglesias <slopeziglesias@danielcastelao.org>
2023-05-24 11:42:10 +0200

    actualizando javadoc de la clase Coche

--- src/Coche.java
+++ src/Coche.java
@@ -3,6 +3,11 @@ public class Coche {
     String modelo;
     Integer velocidad;
 
+    /**
+     * Constructor
+     * @param modelo del coche
+     * @param matricula del coche
+     */
     public Coche(String modelo, String matricula) {
         this.modelo = modelo;
         this.matricula = matricula;
