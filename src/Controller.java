211de344ae68818d123b9c655d4d4ac63c7d3c29
JorgeUsatorre <usatorre1210@gmail.com>
2023-05-24 08:47:34 +0200

    Commit codigo

--- src/Controller.java
+++ src/Controller.java
@@ -2,22 +2,33 @@ import java.lang.module.ModuleDescriptor;
 
 public class Controller {
     public static void main(String[] args) {
+        Model nuevModelo = new Model();
+        View nuevVista = new View();
 
         // Crear tres coches
 
-        Model.crearCoche("LaFerrari", "SBC 1234");
-        Model.crearCoche("Alpine", "HYU 4567");
-        Model.crearCoche("Aston Martin", "FGH 3333");
+        nuevModelo.crearCoche("LaFerrari", "SBC 1234");
+        nuevModelo.crearCoche("Alpine", "HYU 4567");
+        nuevModelo.crearCoche("Aston Martin", "FGH 3333");
 
-        Coche ferrari = Model.getCoche("SBC 1234");
+        Coche ferrari = miModelo.getCoche("SBC 1234");
+
         // modifica la velocidad
-        Model.cambiarVelocidad("SBC 1234", 30);
-
+        miModelo.cambiarVelocidad("SBC 1234", 20);
         // recoje la velocidad y la muestra (tarea de la View)
-        boolean hecho = View.muestraVelocidad("SBC 1234", Model.getVelocidad("SBC 1234"));
-
+        boolean hecho = miVista.muestraVelocidad("SBC 1234", miModelo.getVelocidad("SBC 1234"));
         System.out.println(hecho);
 
-        System.out.println("Prueba");
+        // sube la velocidad
+        miModelo.subirVelocidad("SBC 1234", 200);
+        // recoje la velocidad y la muestra (tarea de la View)
+        hecho = miVista.muestraVelocidad("SBC 1234", miModelo.getVelocidad("SBC 1234"));
+        System.out.println(hecho);
+
+        // baja la velocidad
+        miModelo.bajarVelocidad("SBC 1234", 80);
+        // recoje la velocidad y la muestr (tarea de la View)
+        hecho = miVista.muestraVelocidad("SBC 1234", miModelo.getVelocidad("SBC 1234"));
+        System.out.println(hecho);
     }
 }
