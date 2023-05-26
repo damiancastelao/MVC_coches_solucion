e7d5cb9eb580b032231691208414eefd5a4b3c1f
Lucas <114582569+lucasffffff@users.noreply.github.com>
2023-05-26 09:46:23 +0200

    Update ObsExceso.java
    "Hago que el aviso de velocidad alta salga en pantalla, no solo en consola

--- src/ObsExceso.java
+++ src/ObsExceso.java
@@ -1,12 +1,13 @@
 import java.util.Observable;
 import java.util.Observer;
+import javax.swing.JOptionPane;
 
 public class ObsExceso implements Observer {
     @Override
     public void update(Observable o, Object arg) {
         Coche coche = (Coche) arg;
         if (coche.getVelocidad() > 120) {
-            System.out.println("¡Alerta: velocidad excedida!");
+            JOptionPane.showMessageDialog(null, "¡Alerta: velocidad sobrepasada!", "Aviso", JOptionPane.WARNING_MESSAGE);
         }
     }
 }
