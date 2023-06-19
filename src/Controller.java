9a985d3ef9c801a85f13c190fdd39d0a69855d0c
Miguii-6 <mmarinomartinez@danielcastelao.org>
2023-06-19 03:41:29 +0200

    Refago o buscar coche, e puño java doc

--- src/Controller.java
+++ src/Controller.java
@@ -4,18 +4,29 @@ public class Controller {
     public static void main(String[] args) {
         IU.crearVentana();
     }
+    /**
+     * Método para crear un nuevo coche
+     * @param modelo -> modelo del coche
+     * @param matricula -> matricula que tiene el coche
+     */
     public static void crearCoche(String modelo, String matricula){
         Coche aux = miModelo.crearCoche(modelo,matricula);
         if(aux!=null){
             miVista.muestraVelocidad(aux.matricula, aux.velocidad);
         }
     }
-
+    /**
+     * Método bajar velocidad de un coche
+     * @param matricula -> matricula del coche a disminuir su velocidad
+     */
     public static void bajarVelocidad(String matricula){
         int aux = miModelo.bajarVelocidad(matricula);
         miVista.muestraVelocidad(matricula, aux);
     }
-
+    /**
+     * Método aumentar velocidad de un coche
+     * @param matricula -> matricula del coche a aumentar su velocidad
+     */
     public static void aumentarVelocidad(String matricula){
         int aux = miModelo.subirVelocidad(matricula);
         miVista.muestraVelocidad(matricula,aux);
@@ -24,19 +35,16 @@ public class Controller {
     //empezamos o examen
 
     /**
-     * Devolve os datos do coche pola matricula
-     * @param matricula
-     * @return
-
+     * Método buscar coche que mostre o coche si existe
+     * ou que mostre que non haxa coche
+     * @param matricula -> matricula do coche
+     * Colle o metodo getCoche do Model e ao mostrarCoche da Viw
      */
-    public static void buscarCoche(String matricula) {
-
+    public static void buscarCoche(String matricula){
+        //recolle el coche do modelo
         Coche aux = miModelo.getCoche(matricula);
-        if (aux != null) {
-            miVista.mostrarCoche(aux.matricula, aux.modelo, aux.velocidad);
-        } else {
-            Dialog.msjDialog("Coche no encontrado.");
-        }
+        //chama a vista para que mostre o coche
+        miVista.mostrarCoche(aux, matricula);
     }
 
 }
--- src/View.java
+++ src/View.java
@@ -1,12 +1,26 @@
 public class View {
+    /**
+     * Método ver velocidad actual del coche
+     * @param matricula -> matricula del coche
+     * @param v -> velocidad del coche
+     */
     void muestraVelocidad(String matricula, Integer v){
         Dialog.msjDialog("El coche " + matricula + " tiene una velocidad de: " + v + "km/h");
     }
 
-    // creo mostrar coche para que despois me aparezco o resultado ao darlle oo boton
-
-    void mostrarCoche(String matricula, String modelo, Integer v){
-        Dialog.msjDialog("El coche de este modelo:" + modelo+ " e desta matricula "+ matricula + " y  tiene una velocidad de: " + v + "km/h");
+    /**
+     * Metodo para ver o coche que mostra o coche
+     * ou que diga que non existe o coche
+     * @param aux -> auxiliar do coche
+     * @param matricula -> matricula do coche (por si non existe)
+     */
+    void mostrarCoche(Coche aux, String matricula){
+        if(aux==null){
+            Dialog.msjDialog("Non existe o coche ca matrícula: " + matricula);
+        }
+        else{
+            Dialog.msjDialog("O coche co  " + aux.modelo + " coa matrícula: " + aux.matricula + " ten unha velocidad de: " + aux.velocidad +"km/h");
+        }
     }
 }
     
