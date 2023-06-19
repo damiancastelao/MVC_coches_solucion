8f784efdf6611b7c64f23350660a2f41d4cc21f7
Miguii-6 <mmarinomartinez@danielcastelao.org>
2023-06-19 03:27:43 +0200

    Javadoc de la clase coche (Empezo coa corrección)

--- src/Coche.java
+++ src/Coche.java
@@ -2,6 +2,11 @@ public class Coche {
     String matricula;
     String modelo;
     Integer velocidad;
+    /**
+     * Constructor de Coche en el que tiene la velocidad por defecto (0)
+     * @param modelo -> modelo del coche
+     * @param matricula -> matrícula del coche
+     */
 
     public Coche(String modelo, String matricula) {
         this.modelo = modelo;
